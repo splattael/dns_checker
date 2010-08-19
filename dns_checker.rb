9de29e3562df9dc5c0aefb670c73695fe075fb45
Peter Suschlik <peter@suschlik.de>
2010-08-19 23:50:45 +0200

    Checker is now a class.

--- dns_checker.rb
+++ dns_checker.rb
@@ -8,69 +8,85 @@ Bundler.setup
 require 'net/dns/resolver'
 require 'term/ansicolor'
 
-COLORS = {
-  :fail => :red,
-  :pass => :green,
-  :warn => :yellow
-}
+ROOT_NAMESERVER = "a.root-servers.net"
 
 class String
   include Term::ANSIColor
 end
 
-def resolve(domain, resolver=nil)
-  resolver ||= Net::DNS::Resolver.new
-  resolver.search(domain)
-end
+class DNSChecker
+  COLORS = {
+    :fail => :red,
+    :pass => :green,
+    :warn => :yellow
+  }
 
-def resolve_at(domain, nameserver)
-  resolver = Net::DNS::Resolver.new
-  resolver.nameservers = nameserver
-  resolver.search(domain)
-end
-
-def resolve_address(domain)
-  resolve(domain).answer.first.address.to_s
-end
-
-def resolve_root_nameserver(domain)
-  tld = domain.split(/\./)[-1]
-  @cache ||= {}
-  @cache[tld] ||= resolve_at(domain, ROOT_NAMESERVER).additional.select { |a| Net::DNS::RR::A === a }.choice.address.to_s
-end
-
-def extract_ns(authority)
-  authority.select { |a| Net::DNS::RR::NS === a }.map(&:nsdname).sort.first
-end
-
-def check(domain)
-  nameserver = resolve_root_nameserver(domain)
-
-  at_root = extract_ns(resolve_at(domain, nameserver).authority)
-  local = extract_ns(resolve_at(domain, MY_NAMESERVER).authority)
-
-  if at_root != local
-    write :fail, domain, "'#{at_root}' != '#{local}'"
-  else
-    write :pass, domain
+  def initialize(root_nameserver, local_nameserver)
+    @root_nameserver = resolve_address(root_nameserver)
+    @local_nameserver = resolve_address(local_nameserver)
+    @tld_cache = {}
   end
-rescue => e
-  write :warn, domain, e
+
+  def resolve(domain, resolver=nil)
+    resolver ||= Net::DNS::Resolver.new
+    resolver.search(domain)
+  end
+
+  def resolve_at(domain, nameserver)
+    resolver = Net::DNS::Resolver.new
+    resolver.nameservers = nameserver
+    resolver.search(domain)
+  end
+
+  def resolve_address(domain)
+    resolve(domain).answer.first.address.to_s
+  end
+
+  def resolve_root_nameserver(domain)
+    @tld_cache[tld(domain)] ||= extract_a(resolve_at(domain, @root_nameserver).additional)
+  end
+
+  def tld(domain)
+    domain.split(/\./)[-1]
+  end
+
+  def extract_a(additional)
+    additional.select { |a| Net::DNS::RR::A === a }.choice.address.to_s
+  end
+
+  def extract_ns(authority)
+    authority.select { |a| Net::DNS::RR::NS === a }.map(&:nsdname).sort.first
+  end
+
+  def check(domain)
+    nameserver = resolve_root_nameserver(domain)
+
+    at_root = extract_ns(resolve_at(domain, nameserver).authority)
+    local = extract_ns(resolve_at(domain, @local_nameserver).authority)
+
+    if at_root != local
+      write :fail, domain, "'#{at_root}' != '#{local}'"
+    else
+      write :pass, domain
+    end
+  rescue => e
+    write :warn, domain, e
+  end
+
+  def write(type, domain, reason=nil)
+    color = COLORS[type]
+    reason = ": #{reason.to_s.cyan.bold}" if reason
+    "#{type.to_s.upcase.send(color).bold} #{domain}#{reason}"
+  end
 end
 
-def write(type, domain, reason=nil)
-  color = COLORS[type]
-  reason = ": #{reason.to_s.cyan.bold}" if reason
-  puts "#{type.to_s.upcase.send(color).bold} #{domain}#{reason}"
-end
+# Main
 
-my_nameserver = ARGV.shift or abort("usage: echo 'example.com' | #{$0} my-nameserver")
+if $0 == __FILE__
+  local_nameserver = ARGV.shift or abort("usage: echo 'example.com' | #{$0} my-nameserver")
+  checker = DNSChecker.new(ROOT_NAMESERVER, local_nameserver)
 
-ROOT_NAMESERVER = resolve_address("a.root-servers.net")
-MY_NAMESERVER = resolve_address(my_nameserver)
-
-STDIN.each do |domain|
-  domain.chomp!
-
-  check domain
+  STDIN.each do |domain|
+    puts checker.check(domain.chomp)
+  end
 end
